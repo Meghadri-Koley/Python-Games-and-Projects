5334489bf9ad74fd5aba41092f1be838a39139b4
Sreejita2407 <111240907+Sreejita2407@users.noreply.github.com>
2022-10-19 23:18:58 +0530

    Update Dice_Roll_Generator.py

--- Dice Roll Generator/Dice_Roll_Generator.py
+++ Dice Roll Generator/Dice_Roll_Generator.py
@@ -1,31 +1,16 @@
+#Rolling the dice
 import random
-
-#Enter the minimum and maximum limits of the dice rolls below
-
-min_val = 1
-
-max_val = 6
-
+min = 1
+max = 6
 #the variable that stores the user’s decision
-
-roll_again = "YES"
-
+roll_again = "yes"
 #The dice roll loop if the user wants to continue
-
-while roll_again == "YES" or roll_again == "Y":
-
-print("Dices rolling...")
-
-print("The values are :")
-
+while roll_again == "yes" or roll_again == "y":
+    print ("Rolling the dices...")
+    print ("The values are....")
 #Printing the randomly generated variable of the first dice
-
-print(random.randint(min_val, max_val))
-
+    print (random.randint(min, max))
 #Printing the randomly generated variable of the second dice
-
-print(random.randint(min_val, max_val))
-
+    print (random.randint(min, max))
 #Here the user enters yes or y to continue and any other input ends the program
-
-roll_again = input("Roll The Dices Again?")
+    roll_again = input("Roll the dices again?")
